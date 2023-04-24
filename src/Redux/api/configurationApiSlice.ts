33455be20b4f9801c933ea3a625c9ac1a966b8d6
olutunde22 <solabi.tunde@gmail.com>
2023-04-24 12:37:14 +0100

    fix: fallback on configuration

--- src/Redux/api/configurationApiSlice.ts
+++ src/Redux/api/configurationApiSlice.ts
@@ -7,7 +7,7 @@ export const configurationApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         getConfiguration: builder.query<Configuration, undefined>({
             query: () => ({
-                url: `/configuration/${process.env.REACT_APP_APP_ID !== '' ? process.env.REACT_APP_APP_ID : APP_ID}/`,
+                url: `/configuration/${"REACT_APP_APP_ID" in process.env && process.env.REACT_APP_APP_ID !== '' ? process.env.REACT_APP_APP_ID : APP_ID}/`,
                 method: HTTP.GET,
             }),
             providesTags: (_result, _error, id) => [{ type: 'Configuration', id }],
